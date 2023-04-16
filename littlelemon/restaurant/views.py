aacbd227015984958dc567909127bc5744ba63cf
Khoirul Akmal <khoirulakmal.w@gmail.com>
2023-04-16 15:30:34 +0700

    Add Token Auth

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -15,6 +15,7 @@ def index(request):
 class MenuItemsView(generics.ListCreateAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
+    permission_classes = [permissions.IsAuthenticated] 
     
 class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Menu.objects.all()
