e9f54871da42467b2c49c8087dee4c7f613d18af
Khoirul Akmal <khoirulakmal.w@gmail.com>
2023-04-16 09:41:23 +0700

    Added Booking API

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin 
 from django.urls import path 
-from .views import sayHello
 from . import views
   
 urlpatterns = [ 
     path('', views.index, name='index'),
+    path('menu/', views.MenuItemsView.as_view()),
+    path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
 ]
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework import generics, viewsets, permissions
+from .models import *
+from .serializers import *
 
 
 # Create your views here.
@@ -8,3 +11,16 @@ def sayHello(request):
 
 def index(request):
     return render(request, 'index.html', {})
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    permission_classes = [permissions.IsAuthenticated] 
